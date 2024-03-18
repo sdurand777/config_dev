07d904fb1343c6ee15e5b8e5bb272e7ef1bd9ec6
ivm <stan777.durand777@gmail.com>
2024-03-18 18:04:27 +0100

    update submodule git

--- install_packages.sh
+++ install_packages.sh
@@ -120,6 +120,8 @@ distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
       && curl -s -L https://nvidia.github.io/libnvidia-container/$distribution/libnvidia-container.list | \
             sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
             sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
+sudo apt-get update
+sudo ldconfig
 sudo apt-get install -y nvidia-docker2
 
 #sudo apt install nvidia-docker2 -y
