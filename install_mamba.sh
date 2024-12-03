aa69dbf9135c9d8d612f15b745c96eb8ac379b5a
ivm <stan777.durand777@gmail.com>
2024-12-03 10:32:35 +0100

    update git

--- install_mamba.sh
+++ install_mamba.sh
@@ -1,6 +1,5 @@
 # installation de mamba
 export CONDA_DIR="$HOME/conda"
-mkdir -p $HOME/conda
 wget -O $HOME/Miniforge.sh "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-Linux-$(uname -m).sh"
 bash $HOME/Miniforge.sh -b -p $CONDA_DIR
 rm $HOME/Miniforge.sh
